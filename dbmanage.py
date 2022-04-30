fbd98036ccdab64b6a406564b3cb20f69315c389
lampros2003 <46668710+lampros2003@users.noreply.github.com>
2022-04-30 20:32:16 +0300

    password functionality

--- dbmanage.py
+++ dbmanage.py
@@ -6,28 +6,24 @@ import numpy as np
 import pandas as pd
 import dbcreds as creds
 import pandas.io.sql as psql
-username = 'testplayer'
-conn_string = "host="+ creds.PGHOST +" port="+ "5432" +" dbname="+ creds.PGDATABASE +" user=" + creds.PGUSER \
-+" password="+ creds.PGPASSWORD
-conn=psycopg2.connect(conn_string)
+
+def connecttodb():
+    conn_string = "host="+ creds.PGHOST +" port="+ "5432" +" dbname="+ creds.PGDATABASE +" user=" + creds.PGUSER \
+    +" password="+ creds.PGPASSWORD
+    conn=psycopg2.connect(conn_string)
+    
+
+    
+    
+    return conn
 
 
-# Create a cursor object
-cursor = conn.cursor()
+conn = connecttodb()
 
 
-def load_data(schema, table):
 
-    sql_command = "SELECT * FROM {}.{};".format(str(schema), str(table))
-    print (sql_command)
-
-    # Load the data
-    data = pd.read_sql(sql_command, conn)
-
-    print(data.shape)
-    return (data)
-
-def create_tables(cursor):
+def create_tables(conn):
+    cursor =conn.cursor()
     command = """
         CREATE TABLE players (
             player_name VARCHAR(255) NOT NULL,
@@ -42,29 +38,34 @@ def create_tables(cursor):
     
 
     
-def fetchplayer(cursor,pvs):
+def fetchplayer(conn,pvs):
+    cursor =conn.cursor()
     cursor.execute("SELECT * FROM players WHERE player_name = '{}'".format(pvs[0]))
     vals = cursor.fetchall()
     return vals
 
-playervars = ("Joe","12345678",17,18,14,16)
-def insertplayer(cursor,pvs):
+playervars = ("Jane","12345678",19)
+def insertplayer(conn,pvs):
+    cursor =conn.cursor()
     """ insert a  player into the players table """
     sql = """INSERT INTO players (player_name,player_pass,player_tries1,player_tries2,player_tries3,player_tries4) 
     VALUES('{}','{}',{},{},{},{}) ;""".format(pvs[0],pvs[1],pvs[2],0,0,0)
     cursor.execute(sql)
-def editplayer(cursor,pvs):
-    updatevals = fetchplayer(cursor,pvs)
+def editplayer(conn,pvs):
+    cursor =conn.cursor()
+    updatevals = fetchplayer(conn,pvs)
     command = """UPDATE players
     SET player_tries4 = {player_tries3},player_tries3={player_tries2},player_tries2={player_tries1},player_tries1={}
     WHERE player_name = '{}';
     """.format(pvs[2],pvs[0],player_tries3 = updatevals[0][4],player_tries2 = updatevals[0][3],player_tries1 = updatevals[0][2])
     cursor.execute(command)
-
-
-editplayer(cursor,playervars)
-conn.commit()
-print(fetchplayer(cursor,playervars))
-
-
+def manageplayer(conn,pvs):
+    
+    vals = fetchplayer(conn,pvs)
+    if vals:
+       editplayer(conn,pvs)
+       conn.commit()
+    else :
+        insertplayer(conn,pvs)
+        conn.commit()
 
--- pythonquiz.py
+++ pythonquiz.py
@@ -2,28 +2,44 @@ import re
 import os
 import random
 import datetime
-
+import dbmanage
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager
 total_questions = 5
+conn = dbmanage.connecttodb()
 class Quiz:
     allQuiz = {}
     def __init__(self, id, question, replies, correct):
-        ...
+        pass
     def calculate_score(self, reply):
-        ...
+        pass
     def __str__(self):
-        ...
+        pass
 
 class Player:
     players = {}
+    
+    
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+     
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
     @staticmethod
-    def load_players():
+    def load_players(self):
         pass
-    def __init__(self, name, update=True):
-        pass
+    def __init__(self, name, password,update=True):
+        self.set_password(password)
+        self.values = [name,self.password_hash,0]
+        
     def new_game(self, score, update=True):
         pass
     def update_players(self):
-        pass
+        self.set_password(self.values[1])
+        self.values[1] = self.password_hash
+        print(self.values)
+        dbmanage.manageplayer(conn,self.values )
     def __str__(self):
         pass
 
@@ -38,3 +54,11 @@ if __name__ == "__main__":
     pass
     #load_quiz()
     #play_quiz()
+test = Player("kloe","!afefR34")
+test.update_players()
+print(test.password_hash)
+print(check_password_hash (dbmanage.fetchplayer(conn,test.values)[0][1],"!afefR34"))
+
+
+
+
