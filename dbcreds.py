f1bb55600e17316a3cad1419ef535d7cd26d9ced
ΛΑΜΠΡΟΣ ΑΒΟΥΡΗΣ <46668710+lampros2003@users.noreply.github.com>
2025-03-17 22:54:34 +0200

    Update dbcreds.py

--- dbcreds.py
+++ dbcreds.py
@@ -1,4 +1,1 @@
-PGHOST = "ec2-52-30-159-47.eu-west-1.compute.amazonaws.com"
-PGDATABASE="db6ughb7u3k5m4"
-PGUSER="qzgweknlojtcvq"
-PGPASSWORD="e0e79d0530742b53fe6ef53103057009cf8063d0765ae9f99a5b1f86351a35c2"
+
